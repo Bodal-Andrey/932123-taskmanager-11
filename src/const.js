569fd1c0b8625b15188cfd7c4152af0bead09ef1
Bodal-Andrey <a-v-bodal@yandex.ru>
2020-05-06 22:32:11 +0500

    Выполнил задание

--- src/const.js
+++ src/const.js
@@ -1,5 +1,4 @@
 export const DAYS = [`mo`, `tu`, `we`, `th`, `fr`, `sa`, `su`];
-export const COLORS = [`black`, `yellow`, `blue`, `green`, `pink`];
 
 export const MONTH_NAMES = [
   `January`,
@@ -15,3 +14,22 @@ export const MONTH_NAMES = [
   `November`,
   `December`,
 ];
+
+export const FilterType = {
+  ALL: `all`,
+  ARCHIVE: `archive`,
+  FAVORITES: `favorites`,
+  OVERDUE: `overdue`,
+  REPEATING: `repeating`,
+  TODAY: `today`,
+};
+
+export const COLOR = {
+  BLACK: `black`,
+  YELLOW: `yellow`,
+  BLUE: `blue`,
+  GREEN: `green`,
+  PINK: `pink`,
+};
+
+export const COLORS = Object.values(COLOR);
